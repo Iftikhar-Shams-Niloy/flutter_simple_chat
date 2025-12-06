9379157c4c8c3f6d4c999f9326e07a4fb326d528
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-12-06 16:12:23 +0600

    Add authentication screen with email and password fields
    - Implemented AuthScreen as a StatefulWidget
    - Added a logo image at the top of the screen
    - Created a form with email and password input fields

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AushScreenState();
+  }
+}
+
+class _AushScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image.asset("assets/images/simple_chat_logo.png"),
+              ),
+
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsetsGeometry.all(16),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Email Address"
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                          ),
+
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: "Password"
+                            ),
+                            autocorrect: false,
+                            obscureText: true,
+                            textCapitalization: TextCapitalization.none,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
