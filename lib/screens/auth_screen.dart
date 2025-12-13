a2ca90812bc6f79e4154de6a9af6fa2efe5bae11
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-12-14 04:55:39 +0600

    Integrate Firebase authentication and core services into the app

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -16,13 +21,31 @@ class _AushScreenState extends State<AuthScreen> {
   var _enteredEmail = "";
   var _enteredPassword = "";
 
-  void _submit() {
+  void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid) {
+    if (!isValid) {
+      return;
+    } else {
       _form.currentState!.save();
-      print(_enteredEmail);
-      print(_enteredPassword);
+
+      if (_isLogin) {
+        // ...
+      } else {
+        try {
+          final userCredentials = await _firebase
+              .createUserWithEmailAndPassword(
+                email: _enteredEmail,
+                password: _enteredPassword,
+              );
+          print(userCredentials);
+        } on FirebaseAuthException catch (error) {
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(error.message ?? "Authentication Error!")),
+          );
+        }
+      }
     }
   }
 
