99d33e2a2bca124162377b46cbbf2e9174a518e5
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-12-23 15:27:42 +0600

    Implement real-time chat message retrieval and display in chat UI

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,51 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("No Messages Found"));
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("chat")
+          .orderBy("createdAt", descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(child: Text("No Messages Found!!!"));
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+
+        return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 40),
+          itemCount: loadedMessages.length,
+          reverse: true,
+          itemBuilder: (ctx, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue.shade50, Colors.blue.shade100],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.1),
+                    blurRadius: 4,
+                    offset: const Offset(2, 2),
+                  ),
+                ],
+                border: Border.all(color: Colors.blue.shade100, width: 2),
+              ),
+              child: Text(loadedMessages[index].data()["text"]),
+            );
+          },
+        );
+      },
+    );
   }
 }
